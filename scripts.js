6e2a8752d7f959ec2402c230fe3f63ccf0ecc62a
Kryś_master <swiderska.studia@gmail.com>
2018-10-15 23:47:19 +0200

    Prepared form to get values to JavaScript

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
+﻿var database = [];
 /* void */ function initializeDatabase() {}
 
 /* void */ function tryToAddEntry() {
 	var feedback = document.getElementById("Feedback");
 	feedback.innerHTML += "</br> próba dodania wpisu";
 }
-/* void */ function addEntry(name, surname, studies) {}
+/* void */ function addEntry() {
+    F_Name = document.forms[0].F_Name.value;
+    F_Surname = document.forms[0].F_Surname.value;
+    F_Studies = document.forms[0].F_Studies.value;
+    alert("Hello " + F_Name + " " + F_Surname + " " + F_Studies + "!");
+}
 /* bool */ function validateEntry(name, surname, studies) {}
 
 /* string */ function toUpperCase(mixedCaseString) {}
