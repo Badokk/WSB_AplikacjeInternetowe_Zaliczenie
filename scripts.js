e27dfc60bda00b57c2aaa962f32910adce919d7f
Kryś_master <swiderska.studia@gmail.com>
2018-10-16 20:56:30 +0200

    Added database.push and confirm popout

--- scripts.js
+++ scripts.js
@@ -1,17 +1,36 @@
 ﻿var database = [];
-/* void */ function initializeDatabase() {}
+
+window.document.onload = initializeDatabase();
+/* void */ function initializeDatabase() {
+	database.push(["Krystyna", "Świderska", "Informatyka"]);
+	database.push(["a", "a", 1]);
+}
 
 /* void */ function tryToAddEntry() {
+	F_Name = document.forms[0].F_Name.value;
+    F_Surname = document.forms[0].F_Surname.value;
+    F_Studies = document.forms[0].F_Studies.value;
+	if (validateEntry(F_Name, F_Surname, F_Studies)){
+		addEntry(F_Name, F_Surname, F_Studies);
+	} else {
+		var txt;
+		if (confirm("Czy dodać ponownie do bazy danych?")) {
+        addEntry(F_Name, F_Surname, F_Studies);
+		//todo page refresh after confirm - we loose database
+		} else {
+			//do nothing
+		}
+	}
 	var feedback = document.getElementById("Feedback");
 	feedback.innerHTML += "</br> próba dodania wpisu";
 }
-/* void */ function addEntry() {
-    F_Name = document.forms[0].F_Name.value;
-    F_Surname = document.forms[0].F_Surname.value;
-    F_Studies = document.forms[0].F_Studies.value;
-    alert("Hello " + F_Name + " " + F_Surname + " " + F_Studies + "!");
+/* void */ function addEntry(name, surname, studies) {
+	database.push([name, surname, studies]);
+    //alert("Dodano " + [name, surname, studies] + "do bazy danych.");
 }
-/* bool */ function validateEntry(name, surname, studies) {}
+/* bool */ function validateEntry(name, surname, studies) {
+	return false; //Todo find how to use database.includes[[name, surname, studies]]
+}
 
 /* string */ function toUpperCase(mixedCaseString) {}
 
