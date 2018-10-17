868825ea7876555e161c6b681680d94618b48040
Kryś_master <swiderska.studia@gmail.com>
2018-10-17 21:41:36 +0200

    Added add validation and date

--- scripts.js
+++ scripts.js
@@ -2,37 +2,55 @@
 
 window.document.onload = initializeDatabase();
 /* void */ function initializeDatabase() {
-	database.push(["Krystyna", "Świderska", "Informatyka"]);
-	database.push(["a", "a", 1]);
+	database.push(["Krystyna", "Świderska", "Informatyka", "nigdy"]);
+	database.push(["a", "a", 1, "cokolwiek"]);
+	database.push(["a", "b", 1, "cokolwiek"]);
+	database.push(["a", "aa", 1, "cokolwiek"]);
+
 }
 
 /* void */ function tryToAddEntry() {
 	F_Name = document.forms[0].F_Name.value;
     F_Surname = document.forms[0].F_Surname.value;
     F_Studies = document.forms[0].F_Studies.value;
-	if (validateEntry(F_Name, F_Surname, F_Studies)){
-		addEntry(F_Name, F_Surname, F_Studies);
-	} else {
+	if (validateEntry(F_Name, F_Surname)){
 		var txt;
 		if (confirm("Czy dodać ponownie do bazy danych?")) {
-        addEntry(F_Name, F_Surname, F_Studies);
+        addEntry(F_Name, F_Surname, F_Studies, initDate());
 		//todo page refresh after confirm - we loose database
 		} else {
 			//do nothing
 		}
+	} else {
+		addEntry(F_Name, F_Surname, F_Studies, initDate());
 	}
 	var feedback = document.getElementById("Feedback");
 	feedback.innerHTML += "</br> próba dodania wpisu";
 }
-/* void */ function addEntry(name, surname, studies) {
-	database.push([name, surname, studies]);
-    //alert("Dodano " + [name, surname, studies] + "do bazy danych.");
+/* void */ function addEntry(name, surname, studies, date) {
+	database.push([name, surname, studies, date]);
+    alert("Dodano " + [name, surname, studies] + "do bazy danych dnia " + date +".");
 }
-/* bool */ function validateEntry(name, surname, studies) {
-	return false; //Todo find how to use database.includes[[name, surname, studies]]
+/* bool */ function validateEntry(name, surname) {
+	for (var i = 0; i < database.length; i++) {
+		if(database[i][1] == surname) {
+			if(database[i][0] == name) {
+				return true;
+			}
+		}
+	}
+	return false;
 }
 
 /* string */ function toUpperCase(mixedCaseString) {}
+
+/* void */ function initDate() {
+	var date = new Date();
+	var nDate = date.getDate();
+    var nMnth = date.getMonth();
+    var nYear = date.getFullYear();
+	return nDate + "." + nMnth + "." + nYear;
+}
 
 /* void */ function showDatabase() {
 	var feedback = document.getElementById("DbView");
